eb5f336e18942c9f05dd353e39c5908bd6c7a0b3
hisadomi <hb.kmaia93@icloud.com>
2020-08-16 18:20:16 +0900

    先にnameカラムを宣言するよう修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,14 +2,24 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe "名前の表示" do
+    let(:params) {{name: "satou"}}
+
     it "年齢1:ユーザの名前が取得できること" do
-      user = User.new(name: "satou", age: 1)
+      params.merge!(age: 1)
+      user = User.new(params)
       expect(user.disp_name).to eq "satouちゃん"
     end
 
     it "年齢15:ユーザの名前が取得できること" do
-      user = User.new(name: "satou", age: 15)
+      params.merge!(age: 15)
+      user = User.new(params)
       expect(user.disp_name).to eq "satou君"
+    end
+
+    it "年齢30:ユーザの名前が取得できること" do
+      params.merge!(age: 30)
+      user = User.new(params)
+      expect(user.disp_name).to eq "satouさん"
     end
   end
 end
