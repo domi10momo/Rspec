0520af866b7e74f088ea63b1f3daa705e878edc2
hisadomi <hb.kmaia93@icloud.com>
2020-08-16 18:10:49 +0900

    年齢による条件分岐

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "ユーザの名前が取得できること" do
-    user = User.new(name: "satou")
-    expect(user.disp_name).to eq "satouさん"
+  describe "名前の表示" do
+    it "年齢1:ユーザの名前が取得できること" do
+      user = User.new(name: "satou", age: 1)
+      expect(user.disp_name).to eq "satouちゃん"
+    end
+
+    it "年齢15:ユーザの名前が取得できること" do
+      user = User.new(name: "satou", age: 15)
+      expect(user.disp_name).to eq "satou君"
+    end
   end
 end
